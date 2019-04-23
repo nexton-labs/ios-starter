438c1341f24855dc6a73601c82b5adcb413d4610
Rodrigo Gras <rgras@Rodrigos-MacBook-Pro.local>
2019-04-23 10:40:49 -0300

    Initial commit

---
+++ ios-starter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-starter
+//
+//  Created by Rodrigo Gras on 23/04/2019.
+//  Copyright © 2019 Nexton Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
